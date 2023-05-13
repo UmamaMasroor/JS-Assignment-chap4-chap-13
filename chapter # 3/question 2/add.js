02546e9b0e497330fb6cb0b5715864b49cd46622
Umama Masroor <umamafatima319@gmail.com>
2023-05-12 17:08:04 -0700

    question 2

---
+++ chapter # 3/question 2/add.js
@@ -0,0 +1,15 @@
+// question 2
+// Declare 5 legal & 5 illegal variable names.
+
+// legal variables
+let name;
+let myAge;
+let $name;
+let my_age;
+let name1;
+// Illegal variables
+let n a m e; // space in variable is illegal
+let 1name; //number cannot be the initial character
+let @myAge; //can't use @
+let #myname; // can't use any special character other than _ and $
+let if; // variable names cannot be the same as a JavaScript keyword 
