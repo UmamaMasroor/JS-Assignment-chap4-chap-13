672133b12affb81e14222dc3d65f6a98d3b3d3bb
Umama Masroor <umamafatima319@gmail.com>
2023-05-13 22:27:08 -0700

    question 1

---
+++ chapter # 05/add.js
@@ -0,0 +1,7 @@
+// question 1
+// Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+let num1 = 7;
+let num2 = 8;
+let result = num1 + num2;
+document.write("The sum of " + num1 + " and " + num2 + " is " +result );
