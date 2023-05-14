f486593e29f49e96a8312eda2e6c99b68ac96342
Umama Masroor <umamafatima319@gmail.com>
2023-05-13 23:24:13 -0700

    question 4

---
+++ chapter # 05/question # 4/add.js
@@ -0,0 +1,9 @@
+// question 4
+// 4. Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie.
+let ticket = 600;
+let ticket_price = ticket * 5;
+document.write("Total cost to buy 5 tickets to a movie is " +ticket_price+ "PKR");
