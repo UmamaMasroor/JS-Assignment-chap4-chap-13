1e02729d24036562eac1e6063dbbe0ab31648958
Umama Masroor <umamafatima319@gmail.com>
2023-05-13 23:14:03 -0700

    question 3

---
+++ chapter # 05/question # 3/add.js
@@ -0,0 +1,36 @@
+//question 3
+// . Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”.
+let num;
+document.write("Value after variable declaration is " + num + "<br>");
+
+num = 5;
+document.write("Initial value: " + num );
+
+num++;
+document.write("<br>Value after increment is " + num);
+
+num+=7;
+document.write("<br>Value after addition is " + num);
+
+num--;
+document.write("<br>Value after decrement is " + num);
+
+let remainder = num % 3;
+document.write("<br>The remainder is : " + remainder);
