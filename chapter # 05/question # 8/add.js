e7237f8faf536a0a7878a8f65c6ea1bbddadfca5
Umama Masroor <umamafatima319@gmail.com>
2023-05-14 17:17:27 -0700

    question 8

---
+++ chapter # 05/question # 8/add.js
@@ -0,0 +1,13 @@
+// question 8
+// Store total marks & marks obtained by a student in 2
+// variables. Compute the percentage & show the result in
+// your browser
+
+let totalMarks = 980;
+let marksObtained = 902;
+let percentage = (marksObtained / totalMarks) * 100;
+
+document.write("<h2> Marks Sheet </h2>");
+document.write("<br> Total Marks: " +totalMarks);
+document.write("<br> Marks Obtained: " +marksObtained);
+document.write("<br> Percentage: " +percentage);
