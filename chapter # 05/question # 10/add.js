1b0788320ac1664045f88218202c5f49d11783c3
Umama Masroor <umamafatima319@gmail.com>
2023-05-14 17:38:01 -0700

    question 10

---
+++ chapter # 05/question # 10/add.js
@@ -0,0 +1,12 @@
+// question 10
+// Write a program to initialize a variable with some
+// number and do arithmetic in following sequence:
+// a. Add 5
+// b. Multiply by 10
+// c. Divide the result by 2
+// Perform all calculations in a single expression
+
+let num = 9;
+let operation = (num + 5)* 10 / 2;
+document.write ("<h2> Performing Operations </h2>");
+document.write( "After perform all operations , answer is "+operation);
