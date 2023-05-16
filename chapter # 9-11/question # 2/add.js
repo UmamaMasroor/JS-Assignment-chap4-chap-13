fb8285c67d0a796f37b956b340a5b9d5d7bc26fb
Umama Masroor <umamafatima319@gmail.com>
2023-05-15 21:58:28 -0700

    question 2

---
+++ chapter # 9-11/question # 2/add.js
@@ -0,0 +1,13 @@
+// question 2
+// Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+let gender = prompt("Enter your gender");
+if (gender == "male") {
+    alert("Good Morning Sir.");
+    } else if (gender == "female") {
+        alert("Good Morning Ma’am.");
+        } else {
+            alert("Invalid Input.");
+            };
