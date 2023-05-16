ba3be3f9db4d2643308a74ef4a1cdf4663f90cfa
Umama Masroor <umamafatima319@gmail.com>
2023-05-15 23:36:20 -0700

    question 3

---
+++ chapter # 12-13/question # 3/add.js
@@ -0,0 +1,16 @@
+// question 3
+// Write a program that takes input a number from user &
+// state whether the number is positive, negative or zero.
+
+let number = prompt("Enter a number of your choice");
+if(number>0){
+    alert("The number is positive")
+}
+else if(number<0){
+    alert("The number is negative")
+    }
+    else if(number==0){
+        alert("The number is zero")
+        }
+     
+
