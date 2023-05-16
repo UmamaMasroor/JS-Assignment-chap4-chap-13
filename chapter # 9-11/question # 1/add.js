ac0d6e76bb188d2dad30b9f7f7c978ab5d445c54
Umama Masroor <umamafatima319@gmail.com>
2023-05-15 21:53:04 -0700

    question 1

---
+++ chapter # 9-11/question # 1/add.js
@@ -0,0 +1,14 @@
+// question 1
+// Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+let cityName = prompt("Enter your city name:");
+
+if (cityName.toLowerCase() === "karachi") {
+  document.write("Welcome to the city of lights");
+}
+else{
+    document.write("Welcome");
+    
+}
