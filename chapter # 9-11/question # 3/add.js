08daffb0e55ae0ba4b313d4504a363ea3c78a64b
Umama Masroor <umamafatima319@gmail.com>
2023-05-15 22:09:14 -0700

    question 3

---
+++ chapter # 9-11/question # 3/add.js
@@ -0,0 +1,16 @@
+// question 3
+// Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+
+let color1 =prompt("Enter traffic signal color");
+if(color1==="red"){
+    alert("Must stop");
+}
+let color2 =prompt("Enter traffic signal color");
+if(color2==="yellow"){
+    alert("Ready to Move");
+}
+let color3 =prompt("Enter traffic signal color");
+if(color3==="green"){
+    alert("Move Now");
+}
