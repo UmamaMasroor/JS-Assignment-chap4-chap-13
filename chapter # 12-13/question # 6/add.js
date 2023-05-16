599a863756c251a03596ecec7ba51369810b7ed0
Umama Masroor <umamafatima319@gmail.com>
2023-05-16 00:06:57 -0700

    question 6

---
+++ chapter # 12-13/question # 6/add.js
@@ -0,0 +1,19 @@
+// question 6
+// 6. This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// else
+// greeting = "Good evening";
+// }
+
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+}
+else{
+greeting = "Good evening";
+}
+console.log(greeting);
