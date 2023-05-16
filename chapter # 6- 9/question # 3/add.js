adb9efee021eb4fee2b006653ff6355a1bbe3f5d
Umama Masroor <umamafatima319@gmail.com>
2023-05-15 17:35:40 -0700

    question 3

---
+++ chapter # 6- 9/question # 3/add.js
@@ -0,0 +1,5 @@
+// question 3
+// Write a program that takes input a name from user &
+// greet the user.
+let greet = prompt ("What is your name?")
+document.write("Welcome to our website! " +greet);
