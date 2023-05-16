30b9b94c726e9759c1bdd11397ab8357c8a5d4c1
Umama Masroor <umamafatima319@gmail.com>
2023-05-15 23:50:16 -0700

    question 4

---
+++ chapter # 12-13/question # 4/add.js
@@ -0,0 +1,19 @@
+// question 4
+// Write a program that takes a character (i.e. string of
+// length 1) and returns true if it is a vowel, false otherwise
+
+let character = prompt ("Enter a character");
+
+if (
+    character === 'a' ||
+    character === 'e' ||
+    character === 'i' ||
+    character === 'o' ||
+    character === 'u'
+  ) {
+   alert("true");
+}
+else{
+    alert("false");
+    }
+
