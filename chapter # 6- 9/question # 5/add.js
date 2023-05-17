0697ffd642c765e71b9056daf4759a477c52c514
Umama Masroor <umamafatima319@gmail.com>
2023-05-16 20:55:08 -0700

    question 5

--- chapter # 6- 9/question # 5/add.js
+++ chapter # 6- 9/question # 5/add.js
@@ -16,18 +16,18 @@ let subject2 = prompt("Enter 2nd subject name.");
 let subject3 = prompt("Enter 3rd subject name.");
 let total = 100;
 
-let marks1 = prompt("Enter your obtained marks of " +subject1 );
-let marks2 = prompt("Enter your obtained marks of " +subject2 );
-let marks3 = prompt("Enter your obtained marks of " +subject3 );
+let marks1 = parseInt(prompt("Enter your obtained marks of " + subject1));
+let marks2 = parseInt(prompt("Enter your obtained marks of " + subject2));
+let marks3 = parseInt(prompt("Enter your obtained marks of " + subject3));
 let total2 = marks1 + marks2 + marks3;
 
-let percentage = (total* 100) / total2;
+let percentage = (total2 * 100) / (total * 3);
 
 document.write("<table>");
-     document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
 document.write("<tr><td>" + subject1 + "</td><td>" + total + "</td><td>" + marks1 + "</td></tr>");
 document.write("<tr><td>" + subject2 + "</td><td>" + total + "</td><td>" + marks2 + "</td></tr>");
-document.write("<tr><td>" + subject3 + "</td><td>" + total + "</td><td>" +marks3 + "</td></tr>");
+document.write("<tr><td>" + subject3 + "</td><td>" + total + "</td><td>" + marks3 + "</td></tr>");
 document.write("<tr><td><strong>Total</strong></td><td><strong>" + total * 3 + "</strong></td><td><strong>" + total2 + "</strong></td></tr>");
 document.write("<tr><td><strong>Percentage</strong></td><td><strong>" + percentage + "%</strong></td><td></td></tr>");
 document.write("</table>");
