42284dafc28a298a77592d4d258630296c0efdcd
Umama Masroor <umamafatima319@gmail.com>
2023-05-16 21:58:40 -0700

    question 8

---
+++ chapter # 9-11/question # 8/add.js
@@ -0,0 +1,13 @@
+// question 8
+// Write a program to check whether the given number is
+// divisible by 3. Show the message to the user if the number
+// is divisible by 3.
+
+let number = parseInt(prompt("Enter a number:"));
+if (number % 3 == 0) {
+    alert("The number is divisible by 3.");
+    } 
+  else {
+        alert("The number is not divisible by 3.");
+        }
+
